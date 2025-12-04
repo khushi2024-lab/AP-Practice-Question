a560e61f233a52f5df3fff2c3715415f77250395
Khushi Madan <khushimadan19@gmail.com>
2025-12-04 09:51:53 +0530

    done

--- Easy 1/index.js
+++ Easy 1/index.js
@@ -1,5 +1,50 @@
+// # Problem Statement
+
+// Your task is to complete the `writeDataFile()` function, which is inside the **`./index.js`** file.
+
+
+// ##  Requirements
+
+// The function should perform the following:
+
+// - **Write** the text:  
+//   ```
+//   We will not miss the exam next time
+//   ```
+// - into a file named **`file.txt`** in the **same directory** as the `index.js` file.
+// - Use the **absolute path** while writing to the file.
+// - Use the **synchronous method** from the Node.js **`fs`** module.
+
+// ---
+
+// ##  Example
+
+// After calling:
+
+// ```javascript
+// writeDataFile();
+// ```
+
+// A file named **`file.txt`** should exist in the same folder as your `index.js`, containing the text:
+
+// ```
+// We will not miss the exam next time
+// ```
+
+
+// const fs = require("fs");
+// const path = require("path");
+// function writeDataFile() {
+//     fs.writeFileSync(path.join(__dirname, "file.txt"), "We will not miss the exam next time", "utf8");
+
+// }
+// writeDataFile();
+
 const fs = require("fs");
-
+const path = require("path");
 function writeDataFile() {
-
+    const filePath = path.join(__dirname, "file.txt");
+    const data = "We will not miss the exam next time";
+    fs.writeFileSync(filePath, data);
 }
+writeDataFile();
